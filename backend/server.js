8b034eb7f836d534889472fd687194809e5b6333
UTKARSHRAIKWAR <utkarshraikwar555@gmail.com>
2025-03-14 13:09:35 +0530

    Done

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,24 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoute);
 
+// deployment
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is runnig");
+  });
+}
+
+// deployment
+
 app.use(notFound);
 app.use(errorHandler);
 
